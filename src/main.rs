f8bbfeaf9d2f1c3557e613306eff237d4947a37d
Tenerife-Q <2892015852@qq.com>
2026-01-24 00:03:01 +0800

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 // src/main.rs
 
-// 这里的 s01_memory 会自动去寻找 s01_memory/mod.rs
 mod s01_memory;
-// mod s02_abstraction; 
+mod s02_abstraction; // ✅ 取消这里的注释，启用模块
 
 use std::io;
 
 fn main() {
     loop {
         println!("\n=============================================");
-        println!("    🦀 Rust 工程化复习实验室 (v3.0)    ");
+        println!("    🦀 Rust 工程化复习实验室 (v4.0)    ");
         println!("=============================================");
         println!("1. S01: 内存基本法 (Memory)");
-        println!("2. S02: 抽象与契约 (Traits) [锁定]");
+        println!("2. S02: 抽象与契约 (Traits) [已解锁]");
         println!("0. 退出系统");
         println!("请选择板块:");
 
@@ -20,7 +19,8 @@ fn main() {
         io::stdin().read_line(&mut choice).expect("读取失败");
 
         match choice.trim() {
-            "1" => s01_memory::run_experiments(), // 进入二级菜单
+            "1" => s01_memory::run_experiments(),
+            "2" => s02_abstraction::run_experiments(), // ✅ 这里接入 S02
             "0" => {
                 println!("👋 再见!");
                 break;
