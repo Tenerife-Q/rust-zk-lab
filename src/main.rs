3da2cb194d91c0aa020b1d2772ef957a8ee65adf
Tenerife-Q <2892015852@qq.com>
2026-01-30 15:36:40 +0800

    Update main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 mod s01_memory;
 mod s02_abstraction; 
 mod s03_smart_pointers;
+mod s04_concurrency;
 
 use std::io;
 
@@ -14,6 +15,7 @@ fn main() {
         println!("1. S01: 内存基本法 (Memory)");
         println!("2. S02: 抽象与契约 (Traits) [已解锁]");
         println!("3. S03: 智能指针 (Smart Pointers) [已解锁]");
+        println!("4. S04: 并发安全性 (Concurrency) [已解锁]");
         println!("0. 退出系统");
         println!("请选择板块:");
 
@@ -24,6 +26,7 @@ fn main() {
             "1" => s01_memory::run_experiments(),
             "2" => s02_abstraction::run_experiments(), // ✅ 这里接入 S02
             "3" => s03_smart_pointers::run_experiments(), // ✅ 这里接入 S03
+            "4" => s04_concurrency::run_experiments(), // ✅ 这里接入 S04
             "0" => {
                 println!("👋 再见!");
                 break;
